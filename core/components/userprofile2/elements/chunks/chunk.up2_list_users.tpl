4cbbf04e3af14d97b1d11df834e7269fb30ee74a
Володя Володин <vovanblya@mail.ru>
2015-03-31 12:47:14 +0400

    +  [build]

--- core/components/userprofile2/elements/chunks/chunk.up2_list_users.tpl
+++ core/components/userprofile2/elements/chunks/chunk.up2_list_users.tpl
@@ -3,8 +3,8 @@
         <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-6"></div>
-            <div class="col-md-2">Заметки</div>
-            <div class="col-md-2">Комментарии</div>
+            <div class="col-md-2">[[%up2_tickets]]</div>
+            <div class="col-md-2">[[%up2_comments]]</div>
         </div>
         <br>
 
--- core/components/userprofile2/lexicon/ru/front.inc.php
+++ core/components/userprofile2/lexicon/ru/front.inc.php
@@ -1,7 +1,10 @@
 <?php
 
+$_lang['up2_tickets'] = 'Заметки';
+$_lang['up2_comments'] = 'Комментарии';
+
+$_lang['up2_no_tickets'] = 'еще не оставил ни одной заметки';
+$_lang['up2_no_comments'] = 'еще не оставил ни одного комментария';
+
 $_lang['up2_registration'] = 'Регистрация';
 $_lang['up2_lastactivity'] = 'Активность';
-
-$_lang['up2_no_comments'] = 'еще не оставил ни одного комментария';
-$_lang['up2_no_tickets'] = 'еще не оставил ни одной заметки';
