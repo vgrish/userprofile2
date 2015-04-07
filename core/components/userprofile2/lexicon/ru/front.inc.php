910dcef1e794a0c0531532428506baae43b5f646
Володя Володин <vovanblya@mail.ru>
2015-04-07 11:23:31 +0400

    + [build]

--- core/components/userprofile2/lexicon/ru/front.inc.php
+++ core/components/userprofile2/lexicon/ru/front.inc.php
@@ -5,8 +5,6 @@ $_lang['up2_username'] = 'Имя пользователя';
 $_lang['up2_fullname'] = 'Полное имя';
 $_lang['up2_email'] = 'Email';
 $_lang['up2_password'] = 'Пароль';
-
-
 $_lang['up2_avatar'] = 'Аватар';
 $_lang['up2_tickets'] = 'Заметки';
 $_lang['up2_comments'] = 'Комментарии';
@@ -17,23 +15,19 @@ $_lang['up2_logout'] = 'Выход';
 $_lang['up2_registration'] = 'Регистрация';
 $_lang['up2_lastactivity'] = 'Активность';
 
-
 $_lang['up2_no_tickets'] = 'еще не оставил ни одной заметки';
 $_lang['up2_no_comments'] = 'еще не оставил ни одного комментария';
 $_lang['up2_no_favorites'] = 'список избранного пуст';
-
 
 $_lang['up2_section_title_info'] = 'Информация';
 $_lang['up2_section_title_tickets'] = 'Заметки';
 $_lang['up2_section_title_comments'] = 'Комментарии';
 $_lang['up2_section_title_favorites'] = 'Избранное';
 
-
 $_lang['up2_edit_header'] = 'Вы должны заполнить обязательные поля профиля, отмеченные звёздочкой';
 $_lang['up2_remove_avatar'] = 'удалить аватар';
 $_lang['up2_save_profile'] = 'Сохранить';
 $_lang['up2_logout_profile'] = 'Выйти';
-
 
 $_lang['up2_avatar_desc'] = 'Если вы не загрузите собственную картинку, аватар будет получен автоматически с сервиса <a target="_blank" href="http://gravatar.com/">Gravatar</a>';
 $_lang['up2_username_desc'] = 'Имя пользователя, например ivan_petrov';
@@ -41,7 +35,6 @@ $_lang['up2_fullname_desc'] = 'Ваше полное имя, например И
 $_lang['up2_email_desc'] = 'Новый email нужно будет подтвердить.';
 $_lang['up2_specifiedpassword_desc'] = 'Вы можете указать новый пароль.';
 $_lang['up2_confirmpassword_desc'] = 'Нужно повторить новый пароль еще раз, чтобы исключить опечатку.';
-
 
 $_lang['up2_email_subject'] = 'Проверка email';
 $_lang['up2_email_already_exists'] = 'Email уже существует';
@@ -51,4 +44,13 @@ $_lang['up2_profile_success_save'] = 'Профиль успешно сохран
 $_lang['up2_msg_save_email'] = 'Изменения были успешно сохранены. Ваш email не изменится, пока вы его не подтвердите.';
 $_lang['up2_msg_save_noemail'] = 'Изменения были успешно сохранены, но мы не смогли отправить ссылку на новый email.';
 $_lang['up2_msg_avatar_is_remove'] = 'Аватар был удален.';
-
+// date
+$_lang['up2_date_now'] = 'Только что';
+$_lang['up2_date_today'] = 'Сегодня в';
+$_lang['up2_date_yesterday'] = 'Вчера в';
+$_lang['up2_date_tomorrow'] = 'Завтра в';
+$_lang['up2_date_minutes_back'] = '["[[+minutes]] минута назад","[[+minutes]] минуты назад","[[+minutes]] минут назад"]';
+$_lang['up2_date_minutes_back_less'] = 'меньше минуты назад';
+$_lang['up2_date_hours_back'] = '["[[+hours]] час назад","[[+hours]] часа назад","[[+hours]] часов назад"]';
+$_lang['up2_date_hours_back_less'] = 'меньше часа назад';
+$_lang['up2_date_months'] = '["января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря"]';
--- core/components/userprofile2/lexicon/ru/manager.inc.php
+++ core/components/userprofile2/lexicon/ru/manager.inc.php
@@ -7,25 +7,21 @@ $_lang['up2_type_in'] = 'Тип внутренний';
 $_lang['up2_type_out'] = 'Тип внешний';
 $_lang['up2_active'] = 'Включен';
 $_lang['up2_description'] = 'Описание';
+$_lang['up2_type'] = 'Тип';
+$_lang['up2_css'] = 'Css класс';
+$_lang['up2_value'] = 'Значение';
+$_lang['up2_length'] = 'Длина поля';
+$_lang['up2_required'] = 'Обязательно';
+$_lang['up2_editable'] = 'Редактируемое';
+$_lang['up2_description'] = 'Описание';
+$_lang['up2_default'] = 'По-умолчанию';
+$_lang['up2_sections'] = 'Раздел';
+$_lang['up2_search_by_key'] = 'Искать';
 
 $_lang['up2_name_in'] = 'Имя поля';
 $_lang['up2_name_out'] = 'Имя поля в форме';
 $_lang['up2_name_tab_in'] = 'Имя вкладки';
 $_lang['up2_name_tab_out'] = 'Имя вкладки в форме';
-
-
-$_lang['up2_type'] = 'Тип';
-$_lang['up2_css'] = 'Css класс';
-$_lang['up2_value'] = 'Значение';
-$_lang['up2_length'] = 'Длина поля';
-
-
-$_lang['up2_required'] = 'Обязательно';
-$_lang['up2_editable'] = 'Редактируемое';
-$_lang['up2_description'] = 'Описание';
-$_lang['up2_default'] = 'По-умолчанию';
-
-
 
 $_lang['up2_menu_create'] = 'Создать';
 $_lang['up2_menu_add'] = 'Добавить';
@@ -47,19 +43,3 @@ $_lang['up2_btn_cancel'] = 'Отмена';
 // error
 $_lang['up2_type_profile_err'] = 'Неправильный тип профиля!';
 $_lang['up2_required_err'] = 'Заполните обязательные поля!';
-
-
-// date
-$_lang['up2_date_now'] = 'Только что';
-$_lang['up2_date_today'] = 'Сегодня в';
-$_lang['up2_date_yesterday'] = 'Вчера в';
-$_lang['up2_date_tomorrow'] = 'Завтра в';
-$_lang['up2_date_minutes_back'] = '["[[+minutes]] минута назад","[[+minutes]] минуты назад","[[+minutes]] минут назад"]';
-$_lang['up2_date_minutes_back_less'] = 'меньше минуты назад';
-$_lang['up2_date_hours_back'] = '["[[+hours]] час назад","[[+hours]] часа назад","[[+hours]] часов назад"]';
-$_lang['up2_date_hours_back_less'] = 'меньше часа назад';
-$_lang['up2_date_months'] = '["января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря"]';
-
-
-$_lang['up2_sections'] = 'Раздел';
-$_lang['up2_search_by_key'] = 'Искать';
