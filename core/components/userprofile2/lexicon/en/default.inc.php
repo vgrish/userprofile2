9ab3fe2c094bbe830e7292fdfd4d7931b2d81af9
Ivan Bondarenko <ivan.bond@mail.ru>
2015-10-20 14:25:40 +0300

    Update default.inc.php

--- core/components/userprofile2/lexicon/en/default.inc.php
+++ core/components/userprofile2/lexicon/en/default.inc.php
@@ -6,4 +6,28 @@ foreach ($files as $file) {
 }
 
 
-$_lang['userprofile2'] = 'userprofile2';
+$_lang['userprofile2'] = 'UserProfile2';
+$_lang['up2_menu_desc'] = 'Managing User Profiles';
+
+$_lang['up2_settings'] = 'Settings';
+
+$_lang['up2_tabs'] = 'Profile Tabs';
+$_lang['up2_tabs_intro'] = 'Tabs Intro';
+
+$_lang['up2_fields'] = 'Fields';
+$_lang['up2_fields_intro'] = 'Fields Intro';
+
+$_lang['up2_setting'] = 'Settings';
+$_lang['up2_setting_intro'] = 'Settings Intro';
+
+$_lang['up2_lexicon'] = 'Lexicons';
+$_lang['up2_lexicon_intro'] = 'Lexicons Intro';
+
+$_lang['up2_type_field'] = 'Field Types';
+$_lang['up2_type_field_intro'] = 'Intro';
+
+$_lang['up2_type_tab'] = 'Tab Types';
+$_lang['up2_type_tab_intro'] = 'Tab Types Intro';
+
+$_lang['up2_type_profile'] = 'Profile Types';
+$_lang['up2_type_profile_intro'] = 'Profile Types Intro';
